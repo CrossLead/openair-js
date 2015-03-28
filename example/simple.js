60390f25949ced9cde48d432234ef19a6880dce3
Christian Yang <yang.christian@gmail.com>
2015-03-27 17:43:59 -0700

    Add login support, workaround awful `node-soap` namespace limitation

--- example/simple.js
+++ example/simple.js
@@ -13,14 +13,20 @@ var OpenAir = require('../');
 
 var credentials = require('./credentials.json');
 var config = new OpenAir.Configuration(credentials);
+var service = new OpenAir.Service(config);
 
 console.log('Creating OpenAir connection');
 
-config.createConnection()
-  .then(function(client) {
-    console.log('WSDL processed. Service description:');
-    console.log(client.describe());
+service
+  .init()
+  .then(function( /*client*/ ) {
+    console.log('Service initialized. Service description:');
+    console.log(service.config.client.describe());
+    console.log('Last Request:');
+    console.log(service.config.client.lastRequest);
   })
   .catch(function(err) {
     console.error(err);
+    console.error('Last Request:');
+    console.error(service.config.client.lastRequest);
   });
